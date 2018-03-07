149d9656c630e081eff8f83b2caf63c22344e979
Neto Jocelino <netojocelino@gmail.com>
2018-03-07 09:32:35 -0300

    Resposta da diferença do Método de Resolução

---
+++ Q5/Diferenca.pl
@@ -0,0 +1,17 @@
+%% 5 - Quando aplicamos o método da resolução a uma fórmula lógica, %%
+%%% tanto proposicional quanto de predicados, e chegamos em uma cláusula %%%
+%%% vazia, sabemos que a fórmula é insatisfatível. No entanto, %%%
+%%% na programação lógica (que é baseada no método da resolução) dizemos  %%%
+que a consulta é consequência lógica do programa quando encontramos a %%%
+%%% cláusula vazia. Em um caso a cláusula vazia representa contradição e no %%%
+%%% outro representa tautologia. Mostre que não há incoerência entre as duas %%%
+%%% interpretações %%%
+
+
+%%% RESPOSTA %%%
+
+%%   Na lógica proposicional e de predicados, quando aplicamos o Método de
+%%  Resolução, há a negação na fórmula, diferente de quando estamos utilizando
+%%  Programação Lógica. Assim, quando chegamos em uma cláusula vazia na Lógica
+%%  Proposicional ou de Predicados ela significa que há uma insatisfatibilidade,
+%%  diferente do que ocorre com a Programação Lógica.
