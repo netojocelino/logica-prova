bcdd572361db79f89cec02867a913c9af4100776
Neto Jocelino <netojocelino@gmail.com>
2018-03-08 10:28:26 -0300

    Correção em comentário da resposta 5

--- Q5/Diferenca.pl
+++ Q5/Diferenca.pl
@@ -2,7 +2,7 @@
 %%% tanto proposicional quanto de predicados, e chegamos em uma cláusula %%%
 %%% vazia, sabemos que a fórmula é insatisfatível. No entanto, %%%
 %%% na programação lógica (que é baseada no método da resolução) dizemos  %%%
-que a consulta é consequência lógica do programa quando encontramos a %%%
+%%% que a consulta é consequência lógica do programa quando encontramos a %%%
 %%% cláusula vazia. Em um caso a cláusula vazia representa contradição e no %%%
 %%% outro representa tautologia. Mostre que não há incoerência entre as duas %%%
 %%% interpretações %%%
